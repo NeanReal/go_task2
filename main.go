e38a2382ae29c3a831f3eaf6e0d7eda0b0fc99c6
NeanReal <doomfree64@gmail.com>
2026-09-22 22:47:26 +0800

    style correction

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// Package main запускает утилиту для расчета дней до наступления Нового года.
 package main
 
 import (
@@ -10,6 +11,8 @@ func main() {
 	fmt.Printf("До Нового года осталось дней: %d\n", calcDaysToNewYear(now))
 }
 
+// calcDaysToNewYear определяет количество календарных дней, оставшихся
+// от заданной даты t до 1 января следующего календарного года.
 func calcDaysToNewYear(t time.Time) int {
 	start := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC)
 	nextYear := time.Date(t.Year()+1, time.January, 1, 0, 0, 0, 0, time.UTC)
