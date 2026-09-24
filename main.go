b1de724762c5d051b0c7eb1b4f362f2296555ac6
NeanReal <doomfree64@gmail.com>
2026-09-24 23:14:01 +0800

    HTTP API add

--- main.go
+++ main.go
@@ -2,13 +2,43 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
 	"time"
 )
 
 func main() {
-	now := time.Now()
-	fmt.Printf("До Нового года осталось дней: %d\n", calcDaysToNewYear(now))
+	http.HandleFunc("GET /api/calc", func(w http.ResponseWriter, r *http.Request) {
+		dateParam := r.URL.Query().Get("date")
+
+		targetDate := time.Now()
+
+		if dateParam != "" {
+			parsed, err := time.Parse("2006-01-02", dateParam)
+			if err != nil {
+				http.Error(w, "Неверный формат даты. Ожидается YYYY-MM-DD", http.StatusBadRequest)
+				return
+			}
+			targetDate = parsed
+		}
+
+		days := calcDaysToNewYear(targetDate)
+
+		resp := TaskResponse{
+			Date:          targetDate,
+			Title:         "Дни до Нового года",
+			DaysToNewYear: days,
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		err := json.NewEncoder(w).Encode(resp)
+		if err != nil {
+			log.Printf("Ошибка отправки JSON: %v", err)
+		}
+	})
+	log.Println("Сервер работает на http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 // calcDaysToNewYear определяет количество календарных дней, оставшихся
@@ -17,4 +47,10 @@ func calcDaysToNewYear(t time.Time) int {
 	start := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC)
 	nextYear := time.Date(t.Year()+1, time.January, 1, 0, 0, 0, 0, time.UTC)
 	return int(nextYear.Sub(start) / (24 * time.Hour))
+}
+
+type TaskResponse struct {
+	Date          time.Time `json:"date"`
+	Title         string    `json:"title"`
+	DaysToNewYear int       `json:"days_to_new_year"`
 }
